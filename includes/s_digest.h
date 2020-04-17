b5628078c120ce3c2202a779e87beb5e64171e12
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2020-04-17 21:28:25 +0200

    Adding s_digest

---
+++ includes/s_digest.h
@@ -0,0 +1,9 @@
+//
+// Created by silentvoid on 4/17/20.
+// Copyright (c) 2020 SilentVoid. All rights reserved.
+//
+
+#ifndef SILENT_OPENSSL_S_DIGEST_H
+#define SILENT_OPENSSL_S_DIGEST_H
+
+#endif //SILENT_OPENSSL_S_DIGEST_H
---
+++ src/s_digest.c
@@ -0,0 +1,7 @@
+//
+// Created by silentvoid on 4/17/20.
+// Copyright (c) 2020 SilentVoid. All rights reserved.
+//
+
+int s_sha(char *sha_mode, )
+
